def7922c30075dc68dd22760c5449e06fa04b733
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-01-26 19:54:29 -0600

    bad_sum example in Scala
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ bad_sum/bad_sum.scala
@@ -0,0 +1,10 @@
+object BadSum {
+  def main(args: Array[String]) {
+    print("Enter an integer: ")
+    val s = readLine()
+    print("Enter another integer: ")
+    val t = readLine()
+    val sum = s + t;
+    println("They add up to " + sum)
+  }
+}
---
+++ bad_sum/build.sbt
@@ -0,0 +1,3 @@
+name := "bad_sum"
+
+scalaVersion := "2.11.+"
