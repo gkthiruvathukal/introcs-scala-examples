a7f983f3845fd976beddb1d26fe8562d070990cf
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-01-26 20:14:44 -0600

    write_test example in Scala
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ write_test/write_test.scala
@@ -0,0 +1,11 @@
+object WriteTest {
+  def main(args: Array[String]) {
+    val s = "hello"
+    print(s)
+    print("there")
+    println(s)
+    println( "Another line")
+    print("Starting ")
+    println("yet another line")
+  }
+}
