caa0e260ef592976af8bab4e52cdee2245c6d71b
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-01-26 20:02:07 -0600

    update_vars2 example in Scala
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ update_vars2/update_vars2.scala
@@ -0,0 +1,10 @@
+object UpdateVars2 {
+  def main(args: Array[String]) {
+    var a : Int = 5
+    var b : Int = 2 * a
+    a = a * b
+    b = a - b
+    a = a + b
+    println(s"a = $a, b = $b")
+  }
+}
