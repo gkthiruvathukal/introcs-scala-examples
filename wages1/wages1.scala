423a17d15f78f1fa9df00980850977a37261904c
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-01-27 23:06:36 -0600

    wages1 example with UI dependency fetched from our bintray setup
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ wages1/wages1.scala
@@ -0,0 +1,20 @@
+import edu.luc.cs.ui.promptDouble
+
+object Wages1 {
+  def calcWeeklyWages(totalHours : Double, hourlyWage : Double) = {
+     if (totalHours <= 40) {
+       hourlyWage*totalHours;
+     }
+     else {
+       val overtime = totalHours - 40;
+       hourlyWage*40 + (1.5*hourlyWage)*overtime;
+     }
+  }
+
+  def main(args : Array[String]) {
+     val hours = promptDouble("Enter hours worked: ", 0);
+     val wage = promptDouble("Enter dollars paid per hour: ", 0);
+     val total = calcWeeklyWages(hours, wage);  //before chunk2
+     println(f"Wages for $hours hours at $wage%.2f per hour are $total%.2f.")
+   }
+}
