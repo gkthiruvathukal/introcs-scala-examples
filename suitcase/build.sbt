40845177b65c20cc9d46bd2afb6311befa82681c
gkthiruvathukal <gkt@cs.luc.edu>
2015-02-01 20:38:38 +0000

    suitecase (cs -> scala)
    Signed-off-by: gkthiruvathukal <gkt@cs.luc.edu>

--- suitcase/build.sbt
+++ suitcase/build.sbt
@@ -1,3 +1,7 @@
 name := "suitcase"
 
-scalaVersion := "2.11.+"
+scalaVersion := "2.11.5"
+
+resolvers += "loyolachicagocode@bintray" at "http://dl.bintray.com/loyolachicagocode/maven"
+
+libraryDependencies += "edu.luc.cs" %% "introcs-scala-ui" % "0.1.11"
--- suitcase/suitcase.scala
+++ suitcase/suitcase.scala
@@ -1,5 +1,14 @@
+import edu.luc.cs.ui.promptDouble
+
 object suitcase {
-  def main(args: Array[String]) {
-     // Put code here
-  }
+
+  def main(args: Array[String]) { //main chunk      
+    val weight = promptDouble("How many pounds does your suitcase weigh? ", -1.0)
+    if (weight > 50) {
+        println("There is a $25 charge for luggage that heavy.");
+    }
+    if (weight > 0) {
+        println("Thank you for your business.");
+    }                                       
+  } // past main chunk
 }
