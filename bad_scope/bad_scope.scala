9a5e84e1e534ab3ebfb278d948153d40ff68d148
gkthiruvathukal <gkt@cs.luc.edu>
2015-02-01 23:03:51 +0000

    bad_scope (cs -> scala)
    Signed-off-by: gkthiruvathukal <gkt@cs.luc.edu>

--- bad_scope/bad_scope.scala
+++ bad_scope/bad_scope.scala
@@ -1,5 +1,13 @@
-object bad_scope {
+object BadScope {
   def main(args: Array[String]) {
+     val x = 3
+     F()
      // Put code here
+  }
+
+  def F()
+  {
+     // uncomment the following line to get an compile (syntax) error
+     // println(x)
   }
 }
