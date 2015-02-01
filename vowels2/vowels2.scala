2d97165fffa6aca222bab9d68157d289c6604019
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-02-01 13:04:48 -0600

    vowels2 (cs -> scala)
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ vowels2/vowels2.scala
@@ -0,0 +1,20 @@
+import edu.luc.cs.ui._
+
+object Vowels2 {
+   def main(args: Array[String]) {
+      val s = promptLine("Enter a line: ")
+      printVowels(s)
+   } 
+      //                                         new chunk
+      /// Print the vowels (aeiou) in s, one per line.
+   def printVowels(s : String) {
+      var i = 0
+      while (i < s.length) {
+         val vowels = "aeiouAEIOU"
+         if (vowels.contains(s(i))) {
+            println(s(i))
+         }
+         i = i + 1
+      }
+   }
+}
