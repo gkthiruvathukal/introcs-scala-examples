10c2efdee70371193985d5ee1a5f3924413cf6fa
gkthiruvathukal <gkt@cs.luc.edu>
2015-02-02 01:19:39 +0000

    good_scope (cs -> scala)
    Signed-off-by: gkthiruvathukal <gkt@cs.luc.edu>

---
+++ good_scope2/build.sbt
@@ -0,0 +1,3 @@
+name := "good_scope2"
+
+scalaVersion := "2.11.+"
---
+++ good_scope2/good_scope2.scala
@@ -0,0 +1,12 @@
+object GoodScope2 {
+  def main(args: Array[String]) {
+     val z = 3
+     F(x)
+     // Put code here
+  }
+
+  def F(x : Int)
+  {
+     println(x)
+  }
+}
