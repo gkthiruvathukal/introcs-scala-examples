4eda5a96d9a26fba3b0ca4236ae7c03c8ffded07
George K Thiruvathukal <gkt@mininuevo.local>
2015-02-12 16:49:32 -0600

    sum_file: An example of file I/O with Try and Option[Int]
    Signed-off-by: George K Thiruvathukal <gkt@mininuevo.local>

--- sum_file/sum_file.scala
+++ sum_file/sum_file.scala
@@ -1,5 +1,25 @@
-object sum_file {
+import scala.io._
+import scala.util.{Try, Success, Failure}
+import edu.luc.cs.ui._
+
+object SumFile {
   def main(args: Array[String]) {
-     // Put code here
+  	val filename = promptLine("Enter a file of integers: ")
+  	val fileSum = CalcSum(filename)
+  	println(s"Sum is $fileSum")
+  }
+
+  def CalcSum(filename : String) : Int = {
+  	var sum = 0
+  	for (file <- Try(Source.fromFile(filename)).toOption) {
+  		println("Reading $filename")
+  		for (line <- file.getLines) {
+  			val amountOpt = Try(line.toInt)
+  			val amount = amountOpt.getOrElse(0)
+  			println(s"$line = $amount (0 if not an Int)")
+  			sum = sum + amount
+  		}
+  	}
+  	sum
   }
 }
