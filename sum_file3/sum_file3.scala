a3ee47c7e0f5f703e68ed8ebdad09f1fb48f0cd3
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-02-12 20:22:30 -0600

    sum_file3: Fix formatting issue
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- sum_file3/sum_file3.scala
+++ sum_file3/sum_file3.scala
@@ -4,14 +4,14 @@ import edu.luc.cs.ui._
 
 object SumFile3 {
   def main(args: Array[String]) {
-  	val filename = promptLine("Enter a file of integers: ")
-  	CalcSum(filename) match {
-      case (succeeded, sum) =>
-        if (succeeded) {
-          println(s"Sum is $sum")
-        } else {
-          println(s"Could not open $filename")
-        }
+      val filename = promptLine("Enter a file of integers: ")
+      CalcSum(filename) match {
+        case (succeeded, sum) =>
+          if (succeeded) {
+            println(s"Sum is $sum")
+          } else {
+            println(s"Could not open $filename")
+          }
     }
   }
 
