eed937aaa4a5fd178624ce98be860f6613bc2725
gkthiruvathukal <gkt@cs.luc.edu>
2015-02-13 17:05:13 +0000

    constant: ported to Scala
    Signed-off-by: gkthiruvathukal <gkt@cs.luc.edu>

--- constant/constant.scala
+++ constant/constant.scala
@@ -1,5 +1,18 @@
 object constant {
+   // constant, value not reset
+  val PI = 3.14159265358979
+
+  def circleArea(radius : Double) : Double = {
+     PI*radius*radius
+  }
+
+  def circumference(radius : Double) : Double = {
+     return 2*PI*radius
+  }
+
   def main(args: Array[String]) {
-     // Put code here
+     val radius=5
+     println(s"circle area (radius=$radius) = ${circleArea(radius)}")
+     println(s"circumference (radius=$radius) = ${circumference(radius)}")
   }
 }
