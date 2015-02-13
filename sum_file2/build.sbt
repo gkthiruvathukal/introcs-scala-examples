9bc7408e2006d2f705ea88c4230e48d61219cb52
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-02-12 20:07:41 -0600

    sum_file2: How to do file I/O sans exceptions, and a bit of introductory functional thinking
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ sum_file2/build.sbt
@@ -0,0 +1,7 @@
+name := "calc_sum"
+
+scalaVersion := "2.11.5"
+
+resolvers += "loyolachicagocode@bintray" at "http://dl.bintray.com/loyolachicagocode/maven"
+
+libraryDependencies += "edu.luc.cs" %% "introcs-scala-ui" % "0.1.11"
---
+++ sum_file2/sum_file2.scala
@@ -0,0 +1,25 @@
+import scala.io._
+import scala.util.{Try, Success, Failure}
+import edu.luc.cs.ui._
+
+object SumFile2 {
+  def main(args: Array[String]) {
+  	val filename = promptLine("Enter a file of integers: ")
+  	val result = CalcSum(filename)
+    val succeeded = result._1
+    val sum = result._2
+    if (succeeded) {
+    	println(s"Sum is $sum")
+    } else {
+      println(s"Could not open $filename")
+    }
+  }
+
+  def CalcSum(filename : String) : (Boolean, Int) = {
+    val tryFile = Try(Source.fromFile(filename))
+    tryFile match {
+      case Success(file) => (true, file.getLines flatMap { n => Try(n.toInt).toOption } sum)
+      case Failure(file) => (false, 0)
+    }
+  }
+}
