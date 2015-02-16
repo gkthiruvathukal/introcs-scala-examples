9cf75cdfebc470c6ad3e88f9c5b95ca4c4421431
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-02-15 20:13:20 -0600

    string_play: ported to Scala
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- string_play/string_play.scala
+++ string_play/string_play.scala
@@ -1,5 +1,15 @@
 object string_play {
   def main(args: Array[String]) {
-     // Put code here
+		println("Enter three lines:")
+		val line1 = readLine()
+		val line2 = readLine()
+		val line3 = readLine()
+
+		println("Lines in opposite order:")
+		println(line3)
+    println(line2)
+    println(line1)
+
+    println("First line in upper case:  " + line1.toUpperCase)
   }
 }
