59c018705d4ae1d3fc48e83da5a9b1e5a505c575
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-02-17 22:44:27 -0600

    Scalariform reformatting
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- sum_file/sum_file.scala
+++ sum_file/sum_file.scala
@@ -1,25 +1,25 @@
 import scala.io._
-import scala.util.{Try, Success, Failure}
+import scala.util.{ Try, Success, Failure }
 import edu.luc.cs.ui._
 
 object SumFile {
   def main(args: Array[String]) {
-  	val filename = promptLine("Enter a file of integers: ")
-  	val fileSum = CalcSum(filename)
-  	println(s"Sum is $fileSum")
+    val filename = promptLine("Enter a file of integers: ")
+    val fileSum = CalcSum(filename)
+    println(s"Sum is $fileSum")
   }
 
-  def CalcSum(filename : String) : Int = {
-  	var sum = 0
-  	for (file <- Try(Source.fromFile(filename)).toOption) {
-  		println("Reading $filename")
-  		for (line <- file.getLines) {
-  			val amountOpt = Try(line.toInt)
-  			val amount = amountOpt.getOrElse(0)
-  			println(s"$line = $amount (0 if not an Int)")
-  			sum = sum + amount
-  		}
-  	}
-  	sum
+  def CalcSum(filename: String): Int = {
+    var sum = 0
+    for (file <- Try(Source.fromFile(filename)).toOption) {
+      println("Reading $filename")
+      for (line <- file.getLines) {
+        val amountOpt = Try(line.toInt)
+        val amount = amountOpt.getOrElse(0)
+        println(s"$line = $amount (0 if not an Int)")
+        sum = sum + amount
+      }
+    }
+    sum
   }
 }
