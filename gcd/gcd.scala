8fdf9fac4b2ff68d6a244b6f894ee2f677ea1a01
gkthiruvathukal <gkt@cs.luc.edu>
2015-02-23 02:58:41 +0000

    Euclid (recursive)
    Signed-off-by: gkthiruvathukal <gkt@cs.luc.edu>

---
+++ gcd/gcd.scala
@@ -0,0 +1,18 @@
+import scala.util._
+
+object gcd {
+
+  def euclid(x: Int, y: Int): Int = {
+    if (x == 0) y
+    else if (x < 0) euclid(-x, y)
+    else if (y < 0) -euclid(x, -y)
+    else euclid(y % x, x)
+  }
+
+  def main(args: Array[String]) {
+    val a = Try(args(0).toInt).getOrElse(0)
+    val b = Try(args(1).toInt).getOrElse(1)
+    val gcd = euclid(a, b)
+    println(s"euclid($a, $b) = $gcd")
+  }
+}
