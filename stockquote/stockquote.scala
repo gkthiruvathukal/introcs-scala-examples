6f0da3b56f392dc16f8914702cc8086b4f882d34
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-02-26 14:11:49 -0600

    stockquote (simple URL example)
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- stockquote/stockquote.scala
+++ stockquote/stockquote.scala
@@ -1,11 +1,28 @@
+<<<<<<< HEAD
+=======
+import scala.io._
+import scala.util._
+>>>>>>> d8727ea71aac2613d862d4a8567545f22dae1cda
 
 object StockQuote {
   
   def main(args : Array[String]) {
+<<<<<<< HEAD
 
   }
    
 
   :
   Source.fromURL("http://finance.yahoo.com/d/quotes.csv?s=GOOG&f=snbaop11")
+=======
+     val SYMBOL = "GOOG"
+     val symbol = Try(args(0)).getOrElse(SYMBOL)
+ 
+     url = s"http://finance.yahoo.com/d/quotes.csv?s=$symbol&f=snbaop11")
+     data = Source.fromURL(url)
+     for (datum <- data.getLines)
+        println(datum)
+     
+  }
+>>>>>>> d8727ea71aac2613d862d4a8567545f22dae1cda
 }
