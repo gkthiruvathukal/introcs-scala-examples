0b21521a6c8a865c2297c06a46a4e490849ce020
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-02-26 15:16:31 -0600

    Yahoo Finance example (daily readings by dates)
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- stockquote/stockquote.scala
+++ stockquote/stockquote.scala
@@ -3,14 +3,28 @@ import scala.util._
 
 object StockQuote {
   
+  def matchDateForYahoo( dateText : String, m : String, d : String, y : String) : String = {
+  	val date = """(\d\d\d\d)-(\d\d)-(\d\d)""".r
+  	dateText match {
+      case date(year, month, day) => {
+      	s"&${m}=${month.toInt-1}&${d}=${day.toInt}&${y}=${year.toInt}"
+      }
+    }
+  }
+
   def main(args : Array[String]) {
      val SYMBOL = "GOOG"
      val symbol = Try(args(0)).getOrElse(SYMBOL)
- 
-     val url = s"http://finance.yahoo.com/d/quotes.csv?s=$symbol&f=snbaop11"
+
+     val startDate = Try(args(0)).getOrElse("2015-01-01")
+     val endDate = Try(args(1)).getOrElse("2015-02-05")
+
+     val starting = matchDateForYahoo(startDate, "a", "b", "c")
+     val ending = matchDateForYahoo(endDate, "d", "e", "f")
+     val url = s"http://real-chart.finance.yahoo.com/table.csv?s=${symbol}${starting}${ending}&g=d"
+     println(url)
      val data = Source.fromURL(url)
      for (datum <- data.getLines)
         println(datum)
-     
   }
 }
