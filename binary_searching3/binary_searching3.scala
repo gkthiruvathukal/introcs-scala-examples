f0fa55e3e39a182d370c10e668b9add63ec5d3a9
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-03-13 21:45:30 -0500

    side-effect free version of binary searching using iterators and a (fixed-point) state vector
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ binary_searching3/binary_searching3.scala
@@ -0,0 +1,47 @@
+import scala.util.Random.nextInt
+
+case class SearchState( data : Array[Int], item : Int, min : Int, max : Int, foundAt : Int)
+
+object BinarySearching3 {
+
+  def nextState(data: Array[Int], min : Int, max : Int, item : Int) : SearchState = {
+     val mid = (min + max) / 2
+     if (min > max)
+        SearchState(data, item, min, max, -1)
+     else if (item == data(mid)) 
+        SearchState(data, item, mid, mid, mid)
+     else if (item > data(mid))
+        SearchState(data, item, mid+1, max, -1)
+     else
+        SearchState(data, item, min, mid, -1)
+  }
+
+  def gotoNextState(state : SearchState ) = nextState(state.data, state.min, state.max, state.item)
+
+  def intArrayBinarySearch(data: Array[Int], item: Int) : Int = {
+  	val fixedPoint = SearchState( data, item, 0, data.length, -1)
+  	val results = Iterator.iterate(fixedPoint)(gotoNextState) dropWhile( state => state.min < state.max)
+  	val finalState = results.next
+  	finalState.foundAt
+  }
+
+  def main(args: Array[String]) {
+    val n = 10
+    val MaxValue = 100
+    val randInts = Array.fill(n)(nextInt(MaxValue))
+    val sortedInts = randInts.sorted
+    val outcomes = randInts map { item => intArrayBinarySearch(sortedInts, item) }
+    val numberFound = outcomes count { _ >= 0 }
+    if (numberFound == randInts.length)
+      println("All random integers (1st set) were found successfully!")
+
+    // now generate some values that shouldn't be found in the original sortedArray (of randInts)
+    val randInts2 = Array.fill(n)(nextInt(MaxValue) + MaxValue)
+    val outcomes2 = randInts2 map { item => intArrayBinarySearch(sortedInts, item) }
+    val numberFound2 = outcomes2 count { _ >= 0 }
+    if (numberFound2 == 0)
+      println("All random integers (2nd set) were not found successfully")
+
+    println("If you see 3 lines of output, this program works. We will use unit tests next time.")
+  }
+}
