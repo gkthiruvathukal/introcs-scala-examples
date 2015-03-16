03d6b3fc61c6a3fddf7cfb021dce09c741e0ca3f
George K Thiruvathukal <gkt@quince.local>
2015-03-16 08:55:49 -0500

    non-recursive GCD built on states/iterator
    Signed-off-by: George K Thiruvathukal <gkt@quince.local>

---
+++ gcd2/gcd2.scala
@@ -0,0 +1,47 @@
+import scala.util._
+
+case class EuclidState(sign : Int, x : Int, y : Int)
+
+object gcd2 {
+
+  /*
+  Recursive version:
+
+  def euclid(x: Int, y: Int): Int = {
+    if (x == 0) y
+    else if (x < 0) euclid(-x, y)
+    else if (y < 0) -euclid(x, -y)
+    else euclid(y % x, x)
+  }
+  */
+
+  def gotoNextState( state : EuclidState ): EuclidState = {
+    if (state.x == 0)
+       EuclidState(state.sign, 0, state.y)
+    else if (state.x < 0)
+       EuclidState(state.sign, -state.x, state.y)
+    else if (state.y < 0)
+       EuclidState(-state.sign, state.x, -state.y)
+    else
+       EuclidState(state.sign, state.y % state.x, state.x)
+  }
+
+  /*
+   * Version sans recursion!
+   */
+
+  def euclid(x : Int, y : Int): Int = {
+    val fixedPoint = EuclidState(1, x, y)
+    val results = Iterator.iterate(fixedPoint)(gotoNextState) dropWhile (state => state.x != 0)
+    val finalState = results.next
+    finalState.sign * finalState.y
+  }
+
+
+  def main(args: Array[String]) {
+    val a = Try(args(0).toInt).getOrElse(0)
+    val b = Try(args(1).toInt).getOrElse(1)
+    val gcd = euclid(a, b)
+    println(s"euclid($a, $b) = $gcd")
+  }
+}
---
+++ gcd2/gcd_tests.scala
@@ -0,0 +1,22 @@
+import org.scalatest._
+
+import gcd2._
+
+class EuclidTests extends FlatSpec with Matchers {
+
+  "GCD involving 0" should "give y for euclid(0, y)" in {
+    euclid(0, 5) should be(5)
+  }
+
+  it should "give x for euclid(x, 0)" in {
+    euclid(0, 5) should be(5)
+  }
+
+  "GCD not involving 0" should "be 3" in {
+    euclid(3 * 1, 3 * 3) should be(3)
+  }
+
+  it should "be 5" in {
+    euclid(5 * 1, 5 * 5) should be(5)
+  }
+}
