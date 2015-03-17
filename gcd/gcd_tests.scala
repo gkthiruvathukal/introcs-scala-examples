2ac78bcc9b45ca418736125e9aa3a236dd02c753
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-03-17 10:39:57 -0500

    updated unit tests for recursive GCD
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- gcd/gcd_tests.scala
+++ gcd/gcd_tests.scala
@@ -4,19 +4,17 @@ import gcd._
 
 class EuclidTests extends FlatSpec with Matchers {
 
-  "GCD involving 0" should "give y for euclid(0, y)" in {
+  "GCD" should "handle first argument being zero" in {
     euclid(0, 5) should be(5)
   }
 
-  it should "give x for euclid(x, 0)" in {
-    euclid(0, 5) should be(5)
+  it should "handle second argument being zero" in {
+    euclid(5, 0) should be(5)
   }
 
-  "GCD not involving 0" should "be 3" in {
-    euclid(3 * 1, 3 * 3) should be(3)
-  }
-
-  it should "be 5" in {
-    euclid(5 * 1, 5 * 5) should be(5)
+  it should "handle negative arguments properly" in {
+    euclid(-5, 10) should be (5)
+    euclid(5, -10) should be (5)
+    euclid(-5, -10) should be (5)
   }
 }
