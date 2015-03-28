48aa979e5a1fa244d0beb35eaea1c49a4ee5ad2d
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-03-27 22:21:14 -0500

    basic OO example
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- contact1/contact1.scala
+++ contact1/contact1.scala
@@ -1,5 +1,31 @@
+
 object contact1 {
+  class Contact(initName : String, initPhone : String, initEmail : String) {
+    val name = initName
+    val phone = initPhone
+    val email = initEmail
+    override def toString(): String = s"Contact($name, $phone, $email)"
+  }
+  
   def main(args: Array[String]) {
-    // Put code here
+    println("Creating two Contact objects")
+
+    val c1 = new Contact("Marie Ortiz", "773-508-7890", "mortiz2@luc.edu");
+    val c2 = new Contact("Otto Heinz", "773-508-9999", "oheinz@luc.edu");
+
+    println(s"Marie's full name = ${c1.name}")
+    println(s"Marie's phone number = ${c1.phone}")
+    println(s"Marie's e-mail = ${c1.phone}")
+    println(s"Otto's full contact info = ${c1}")
+
+    println("Adding contacts to List[Contact]")
+    val contacts = List(c1, c2)
+
+    println("Printing all contacts in List[Contact]")
+    contacts foreach println
+
+    println("Find contact with a specific phone number")
+    contacts filter { c => c.phone == "773-202-LUNA" } foreach println
+
   }
 }
