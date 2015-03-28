9d9665ad9e790bb711f07025b421347a7d7d3049
George <gkt@cs.luc.edu>
2015-03-28 16:44:27 -0500

    add Scala port of C# version
    Signed-off-by: George <gkt@cs.luc.edu>

--- check_digits1/check_digits1.scala
+++ check_digits1/check_digits1.scala
@@ -1,5 +1,23 @@
+import edu.luc.cs.ui._
+
 object check_digits1 {
   def main(args: Array[String]) {
-    // Put code here
+    val s = promptLine("Enter a line: ")
+    if (isDigits(s))
+       println(s"All characters in $s are digits.")
+    else
+       println(s"Some characters in $s are not digits.")
+  }
+
+  def isDigits(s : String) : Boolean = {
+    var allDigitsSoFar = true
+    var i = 0
+    while (i < s.length && allDigitsSoFar) {
+      if (s(i) < '0' || s(i) > '9') {
+        allDigitsSoFar = false
+      }
+      i += 1
+    }
+    allDigitsSoFar
   }
 }
