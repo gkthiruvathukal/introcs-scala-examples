98f1006bfab8778a703f5c272cc7dde7293f0de3
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-03-28 21:40:24 -0500

    use count() instead of takeWhile()
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- check_digits3/check_digits3.scala
+++ check_digits3/check_digits3.scala
@@ -3,14 +3,14 @@ import edu.luc.cs.ui._
 object check_digits3 {
   def main(args: Array[String]) {
     val s = promptLine("Enter a line: ")
+    val numDigits = countDigits(s)
     if (isDigits(s))
        println(s"All characters in $s are digits.")
     else
-       println(s"Some characters in $s are not digits.")
+       println(s"$numDigits of ${s.length} characters  in $s are digits.")
   }
 
-  def isDigits(s : String) : Boolean = {
-    val digits = s.takeWhile( c => c >= '0' && c <= '9' )
-    digits.length = s.length
-  } 
+  def countDigits(s : String) = s.count( c => c >= '0' && c <= '9' )
+
+  def isDigits(s : String) = countDigits(s) == s.length
 }
