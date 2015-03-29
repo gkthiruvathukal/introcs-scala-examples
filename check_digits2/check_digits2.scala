e6ba0e086021ab1c9a63ee7ea021be8fa162d024
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-03-28 19:08:35 -0500

    for with breakable (not very pretty but just to show it)
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- check_digits2/check_digits2.scala
+++ check_digits2/check_digits2.scala
@@ -1,5 +1,29 @@
+import edu.luc.cs.ui._
+import scala.util.control.Breaks
+
 object check_digits2 {
   def main(args: Array[String]) {
-    // Put code here
+    val s = promptLine("Enter a line: ")
+    if (isDigits(s))
+       println(s"All characters in $s are digits.")
+    else
+       println(s"Some characters in $s are not digits.")
+  }
+
+  def isDigits(s : String) : Boolean = {
+    val myControl = new Breaks()
+    import myControl.{breakable, break}
+
+    var allDigitsSoFar = true
+    breakable {
+      for (i <- 0 until s.length) {
+        println("checking " + s(i))
+        if (s(i) < '0' || s(i) > '9') {
+          allDigitsSoFar = false
+          break()
+        }
+      }
+    }
+    allDigitsSoFar
   }
 }
