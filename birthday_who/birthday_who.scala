2eeafb20408a92db3af21f2b1673bf88d94a1c1c
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-03-28 21:16:20 -0500

    port C# version to Scala
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- birthday_who/birthday_who.scala
+++ birthday_who/birthday_who.scala
@@ -1,5 +1,18 @@
 object birthday_who {
-  def main(args: Array[String]) {
-    // Put code here
-  }
+
+   def happyBirthday(person : String)
+   {
+      println("Happy Birthday to you!")
+      println("Happy Birthday to you!")
+      println("Happy Birthday, dear " + person + ".")
+      println("Happy Birthday to you!")
+   }
+
+   def main(args : Array[String])
+   {
+      println("Who would you like to sing Happy Birthday to?")
+      val userName = readLine()
+      happyBirthday(userName)
+   }
+
 }
