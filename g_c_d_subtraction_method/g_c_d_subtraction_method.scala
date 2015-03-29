eacae97bce64167b3b39cccc795a71f25191b51e
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-03-28 22:19:22 -0500

    port C# version to Scala (with some improved error checking)
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- g_c_d_subtraction_method/g_c_d_subtraction_method.scala
+++ g_c_d_subtraction_method/g_c_d_subtraction_method.scala
@@ -1,5 +1,30 @@
+import edu.luc.cs.ui._
+import math.abs
+
 object g_c_d_subtraction_method {
   def main(args: Array[String]) {
-    // Put code here
+    val a = promptInt("Enter an integer: ", 1)
+    val b = promptInt("Enter another integer: ", 1)
+    val result = gcd(a, b)
+    println(s"gcd($a, $b) = $result (using subtraction method)")
+  }
+
+  def gcd(x : Int, y : Int) : Int = {
+    var a = math.abs(x)
+    var b = math.abs(y)
+    require (a != 0)
+    require (b != 0)
+    var c = 0
+    while (a != b) {
+      while (a > b) {
+        c =  a - b
+        a = c
+      }
+      while (b > a) {
+        c = b - a
+        b = c
+      }
+    }
+    a
   }
 }
