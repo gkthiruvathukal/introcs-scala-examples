e1891ca30675297287635c8a80a4e81304df82cb
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-03-30 10:23:30 -0500

    a bit more to the example
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- point/point1.scala
+++ point/point1.scala
@@ -13,6 +13,7 @@ object point1 {
     
     def translate(dx : Double, dy : Double) = new Point(x + dx, y + dy) 
 
+    override def toString() = s"Point($x, $y)"
   }
   
   def main(args : Array[String]) {
@@ -24,9 +25,17 @@ object point1 {
     println("d(p1,p2) = " + p1.distanceTo(p2))
     println("d(p0,p2) = " + p0.distanceTo(p2))
     
-    p00 = p0.translate(-3, -3)
+    val q0 = p0.translate(-3, -3)
+    val q1 = p1.translate(-3, -3)
+    val q2 = p2.translate(-3, -3)
 
+    println(q0)
+    println(q1)
+    println(q2)
 
+    println("d(p0,p1) = " + p0.distanceTo(p1))
+    println("d(p1,p2) = " + p1.distanceTo(p2))
+    println("d(p0,p2) = " + p0.distanceTo(p2))
   }
 }
   
